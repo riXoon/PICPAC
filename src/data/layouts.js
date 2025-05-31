9915f459aa4681e6394a9c705d4769d8fc9ae47f
Erickson <rixon.code@gmail.com>
2025-05-31 22:29:37 +0800

    ADDED: stored data for the layout

---
+++ src/data/layouts.js
@@ -0,0 +1,29 @@
+import dummy1 from "../assets/images/dummy1.png";
+import dummy2 from "../assets/images/dummy2.png";
+import dummy3 from "../assets/images/dummy3.png";
+import dummy4 from "../assets/images/dummy4.png";
+import dummy5 from "../assets/images/dummy5.png";
+import dummy6 from "../assets/images/dummy6.png";
+import dummy7 from "../assets/images/dummy7.png";
+import dummy8 from "../assets/images/dummy8.png";
+import dummy9 from "../assets/images/dummy9.png";
+import dummy10 from "../assets/images/dummy10.png";
+import dummy11 from "../assets/images/dummy11.png";
+import dummy12 from "../assets/images/dummy12.png";
+import dummy13 from "../assets/images/dummy13.png";
+import dummy14 from "../assets/images/dummy14.png";
+import dummy15 from "../assets/images/dummy15.png";
+
+export const layoutData = {
+  A: [dummy1, dummy2, dummy3],
+  B: [dummy4, dummy5, dummy6, dummy7],
+  C: [dummy8, dummy9],
+  D: [dummy10, dummy11, dummy12, dummy13, dummy14, dummy15],
+};
+
+export const layoutPhotoCount = {
+  A: 3,
+  B: 4,
+  C: 2,
+  D: 6,
+};
