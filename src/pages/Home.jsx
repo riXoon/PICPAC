cdbe5815db78e275713e0ffde48352817ef2f968
Erickson <rixon.code@gmail.com>
2025-05-31 22:29:56 +0800

    ADDED: navigation to start the photobooth

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom';
+
 import StickyNavbar from '../components/StickyNavbar.jsx'
 import HeroPhoto from '../assets/images/hero-photo.png'
 import Logo from '../assets/images/P!CPAC.png'
@@ -8,6 +10,13 @@ import { CiCamera } from "react-icons/ci";
 
 
 function Home() {
+
+  const navigate = useNavigate();
+  
+  const startBooth = () => {
+    navigate('/select-layout')
+  }
+
   return (
     <section className='bg-zinc-50 mt-4 min-h-screen shadow-md flex flex-row justify-center items-center'>
       {/* left container */}
@@ -19,7 +28,7 @@ function Home() {
       <div className='flex flex-col items-center justify-center gap-10'>
         <h1 className = "text-8xl font-bold text-indigo-600">P!CPAC</h1>
         <p className='text-xl w-100 text-center'>turn your browser into a photobooth and get ready to put on your best smile!</p>
-        <button className='text-xl uppercase text-indigo-500 font-bold tracking-wide border-4 border-indigo-500 px-20 py-4 rounded-xl flex items-center gap-4 cursor-pointer transition-transform duration-300 transform hover:scale-105 hover:bg-indigo-50 active:scale-95'>
+        <button onClick={startBooth} className='text-xl uppercase text-indigo-500 font-bold tracking-wide border-4 border-indigo-500 px-20 py-4 rounded-xl flex items-center gap-4 cursor-pointer transition-transform duration-300 transform hover:scale-105 hover:bg-indigo-50 active:scale-95'>
           Start Booth 
           <CiCamera 
           size={30} 
