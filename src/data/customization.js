7101b9cfa4ed4e292847e26b845d2d5152ce0737
Erickson <rixon.code@gmail.com>
2025-06-13 23:32:16 +0800

    REFACTOR: moved my messy imports of assets into seperate js file that will serve as data

---
+++ src/data/customization.js
@@ -0,0 +1,220 @@
+import TemplateA1 from '/templates/LayoutA/layoutA-template1.png';
+import TemplateA2 from '/templates/LayoutA/layoutA-template2.png';
+import TemplateA3 from '/templates/LayoutA/layoutA-template3.png';
+import TemplateA4 from '/templates/LayoutA/layoutA-template4.png';
+import TemplateA5 from '/templates/LayoutA/layoutA-template5.png';
+import TemplateA6 from '/templates/LayoutA/layoutA-template6.png';
+import TemplateA7 from '/templates/LayoutA/layoutA-template7.png';
+import TemplateA8 from '/templates/LayoutA/layoutA-template8.png';
+import TemplateA9 from '/templates/LayoutA/layoutA-template9.png';
+import Template10 from '/templates/LayoutA/layoutA-template10.png';
+import Template11 from '/templates/LayoutA/layoutA-template11.png';
+import Template12 from '/templates/LayoutA/layoutA-template12.png';
+import Template13 from '/templates/LayoutA/layoutA-template13.png';
+
+import OverlayA1 from '/overlays/LayoutA/layoutA-overlay1.png';
+import OverlayA2 from '/overlays/LayoutA/layoutA-overlay2.png';
+import OverlayA3 from '/overlays/LayoutA/layoutA-overlay3.png';
+import OverlayA4 from '/overlays/LayoutA/layoutA-overlay4.png';
+import OverlayA5 from '/overlays/LayoutA/layoutA-overlay5.png';
+import OverlayA6 from '/overlays/LayoutA/layoutA-overlay6.png';
+import OverlayA7 from '/overlays/LayoutA/layoutA-overlay7.png';
+import OverlayA8 from '/overlays/LayoutA/layoutA-overlay8.png';
+import OverlayA9 from '/overlays/LayoutA/layoutA-overlay9.png';
+import OverlayA10 from '/overlays/LayoutA/layoutA-overlay10.png';
+
+import Preview1 from '/overlays/Preview/stickerPreview1.png';
+import Preview2 from '/overlays/Preview/stickerPreview2.png';
+import Preview3 from '/overlays/Preview/stickerPreview3.png';
+import Preview4 from '/overlays/Preview/stickerPreview4.png';
+import Preview5 from '/overlays/Preview/stickerPreview5.png';
+import Preview6 from '/overlays/Preview/stickerPreview6.png';
+import Preview7 from '/overlays/Preview/stickerPreview7.png';
+import Preview8 from '/overlays/Preview/stickerPreview8.png';
+import Preview9 from '/overlays/Preview/stickerPreview9.png';
+import Preview10 from '/overlays/Preview/stickerPreview10.png';
+import Null from '/overlays/Preview/stickerPreviewNull.png';
+
+import FontPreview1 from '/overlays/Preview/FontPreview1.png';
+import FontPreview2 from '/overlays/Preview/FontPreview2.png';
+import FontPreview3 from '/overlays/Preview/FontPreview3.png';
+import FontPreview4 from '/overlays/Preview/FontPreview4.png';
+import FontPreview5 from '/overlays/Preview/FontPreview5.png';
+import FontPreview7 from '/overlays/Preview/FontPreview7.png';
+
+
+export const allTemplate = {
+  A: [
+    { background: TemplateA1 },
+    { background: TemplateA2 },
+    { background: TemplateA3 },
+    { background: TemplateA4 },
+    { background: TemplateA5 },
+    { background: TemplateA6 },
+    { background: TemplateA7 },
+    { background: TemplateA8 },
+    { background: TemplateA9 },
+    { background: Template10},
+    { background: Template11},
+    { background: Template12},
+    { background: Template13 },
+  ],
+  B: [
+    { background: TemplateA1 },
+    { background: TemplateA2 },
+    { background: TemplateA3 },
+    { background: TemplateA4 },
+    { background: TemplateA5 },
+    { background: TemplateA6 },
+    { background: TemplateA7 },
+    { background: TemplateA8 },
+    { background: TemplateA9 },
+    { background: Template10},
+    { background: Template11},
+    { background: Template12},
+    { background: Template13 },
+  ],
+  C: [
+    { background: TemplateA1 },
+    { background: TemplateA2 },
+    { background: TemplateA3 },
+    { background: TemplateA4 },
+    { background: TemplateA5 },
+    { background: TemplateA6 },
+    { background: TemplateA7 },
+    { background: TemplateA8 },
+    { background: TemplateA9 },
+    { background: Template10},
+    { background: Template11},
+    { background: Template12},
+    { background: Template13 },
+  ],
+  D: [
+    { background: TemplateA1 },
+    { background: TemplateA2 },
+    { background: TemplateA3 },
+    { background: TemplateA4 },
+    { background: TemplateA5 },
+    { background: TemplateA6 },
+    { background: TemplateA7 },
+    { background: TemplateA8 },
+    { background: TemplateA9 },
+    { background: Template10},
+    { background: Template11},
+    { background: Template12},
+    { background: Template13 },
+  ],
+};
+
+export const presetColors = [
+  // Light Pastels
+  '#ffffff', '#F2D7D5', '#FAD7A0', '#F9E79F', '#D7BDE2',
+  '#A3E4D7', '#AED6F1', '#E8DAEF', '#FDEBD0', '#E8F8F5',
+
+  // Vivid/Bright
+  '#FF6B6B', '#F39C12', '#5DADE2', '#58D68D', '#AF7AC5',
+
+  // Neutral Tones
+  '#D5DBDB', '#BFC9CA', '#AAB7B8', '#85929E',
+
+  // Dark Colors
+  '#000000', '#2C3E50', '#1C2833', '#4D5656', '#212F3D', '#17202A',
+];
+
+export const presetTextColors = [
+  // Light Pastels
+  '#ffffff', '#AED6F1',
+  // Vivid/Bright
+  '#FF6B6B', '#F39C12',
+  // Neutral Tones
+  '#D5DBDB', '#BFC9CA',
+  // Dark Colors
+  '#000000', '#2C3E50',,
+];
+
+export const allOverlays = {
+  A: [
+    { overlay: null},
+    { overlay: OverlayA1},
+    { overlay: OverlayA2},
+    { overlay: OverlayA3},
+    { overlay: OverlayA4},
+    { overlay: OverlayA5},
+    { overlay: OverlayA6},
+    { overlay: OverlayA7},
+    { overlay: OverlayA8},
+    { overlay: OverlayA9},
+    { overlay: OverlayA10},
+  ],
+  B: [
+    { overlay: null},
+    { overlay: OverlayA1},
+    { overlay: OverlayA2},
+    { overlay: OverlayA3},
+    { overlay: OverlayA4},
+    { overlay: OverlayA5},
+    { overlay: OverlayA6},
+    { overlay: OverlayA7},
+    { overlay: OverlayA8},
+    { overlay: OverlayA9},
+    { overlay: OverlayA10},
+  ],
+  C: [
+    { overlay: null},
+    { overlay: OverlayA1},
+    { overlay: OverlayA2},
+    { overlay: OverlayA3},
+    { overlay: OverlayA4},
+    { overlay: OverlayA5},
+    { overlay: OverlayA6},
+    { overlay: OverlayA7},
+    { overlay: OverlayA8},
+    { overlay: OverlayA9},
+    { overlay: OverlayA10},
+  ],
+  D: [
+    { overlay: null},
+    { overlay: OverlayA1},
+    { overlay: OverlayA2},
+    { overlay: OverlayA3},
+    { overlay: OverlayA4},
+    { overlay: OverlayA5},
+    { overlay: OverlayA6},
+    { overlay: OverlayA7},
+    { overlay: OverlayA8},
+    { overlay: OverlayA9},
+    { overlay: OverlayA10},
+  ]
+}
+
+export const preview = [
+  { preview: Null },
+  { preview: Preview1 },
+  { preview: Preview2 },
+  { preview: Preview3 },
+  { preview: Preview4 },
+  { preview: Preview5 },
+  { preview: Preview6 },
+  { preview: Preview7 },
+  { preview: Preview8 },
+  { preview: Preview9 },
+  { preview: Preview10 },
+]
+
+export const fonts = {
+  Fredoka: 'bold 20px Fredoka, sans-serif',
+  Bungee: 'bold 20px Bungee, sans-serif',
+  Chewy: 'bold 20px Chewy, sans-serif',
+  Luckiest: 'bold 20px Luckiest Guy, sans-serif',
+  Boogaloo: 'bold 20px Boogaloo, sans-serif',
+  Monoton: 'bold 20px Monoton, cursive',
+}
+
+export const fontPreviews = [
+  { preview: FontPreview1, font: fonts.Fredoka },
+  { preview: FontPreview2, font: fonts.Bungee },
+  { preview: FontPreview3, font: fonts.Chewy },
+  { preview: FontPreview4, font: fonts.Luckiest },
+  { preview: FontPreview5, font: fonts.Boogaloo },
+  { preview: FontPreview7, font: fonts.Monoton },
+];
