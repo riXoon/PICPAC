658ba4fbe38e09f5735150444d19f46de08ef252
Erickson <rixon.code@gmail.com>
2025-06-20 21:24:30 +0800

    REFINE: update App component structure to include Contact page and improve layout organization

--- src/App.jsx
+++ src/App.jsx
@@ -6,34 +6,35 @@ import Home from './pages/Home.jsx';
 import About from './pages/About.jsx';
 import Faq from './pages/FAQs.jsx';
 import LayoutSelection from './pages/LayoutSelection.jsx';
-import Photobooth from './pages/Photobooth.jsx'
+import Photobooth from './pages/Photobooth.jsx';
 import CustomizationPage from './pages/CustomizationPage.jsx';
 import PhotoGallery from './pages/PhotoGallery.jsx';
+import Contact from './pages/Contact.jsx';
 
 function App() {
   return (
     <Router>
-      <div className="px-16 py-4">
-      <StickyNavbar />
-        <Routes>
-          <Route 
-            path="/"
-            element={
-              <>
-                <Home />
-                <PhotoGallery />
-                <About />
-              </>
-            }
-          />
-          <Route path="/faq" element={<Faq />} />
-          <Route path="/select-layout" element={<LayoutSelection />} />
-          <Route path="/photobooth" element={<Photobooth />} />
-
-          <Route path="/customization" element={<CustomizationPage />} />
-
-        </Routes>
-      </div>
+        <StickyNavbar />
+        
+        <div className="px-16 py-4">
+          <Routes>
+            <Route 
+              path="/"
+              element={
+                <>
+                  <Home />
+                  <PhotoGallery />
+                  <About />
+                  <Contact />
+                </>
+              }
+            />
+            <Route path="/faq" element={<Faq />} />
+            <Route path="/select-layout" element={<LayoutSelection />} />
+            <Route path="/photobooth" element={<Photobooth />} />
+            <Route path="/customization" element={<CustomizationPage />} />
+          </Routes>
+        </div>
     </Router>
   );
 }
